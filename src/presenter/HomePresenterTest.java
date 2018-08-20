1421fbbb9267b7fc0821a3e0f415b59c479ff6cb
sadra amlashi <amlashi.sadra@gmail.com>
2018-08-20 12:53:29 +0430

    Add red step to test

--- src/presenter/HomePresenterTest.java
+++ src/presenter/HomePresenterTest.java
@@ -6,16 +6,17 @@ class HomePresenterTest {
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        fail("This test has yet to be implemented");
     }
 
     @org.junit.jupiter.api.Test
     void loading() {
-
+        fail("This test has yet to be implemented");
     }
 
     @org.junit.jupiter.api.Test
     void update() {
-
+        fail("This test has yet to be implemented");
     }
 
 }
